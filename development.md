236730945c74c1e915163dd476cac0eaa4aa45d6
Andrew C. Greenberg <wizardwerdna@gmail.com>
2016-05-26 12:05:21 -0700

    installation

---
+++ development.md
@@ -0,0 +1,31 @@
+# FRP Bowling Kata
+
+## Installation
+
+bash
+```bash
+npm install && tsd install
+```
+
+index.html
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <div><h1>FRP Bowling Kata</h1</div>
+  <script src="bundle.js"></script>
+</body>
+</html>
+```
+
+
+app.ts
+```typescript
+console.log('app.ts here');
+```
+
+## TestSuite
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {  
+  entry: './app.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+        loader : 'url?prefix=font/&limit=10000'
+      },
+      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  }
+};
